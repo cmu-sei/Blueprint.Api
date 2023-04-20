1e1de040147f31eabc540dc0f31f0c43e50d08ab
sei-tspencer <trspencer@sei.cmu.edu>
2023-04-20 12:24:41 -0400

    document markings

--- Blueprint.Api.Migrations.PostgreSQL/Migrations/20230419134210_msel-pages.cs
+++ Blueprint.Api.Migrations.PostgreSQL/Migrations/20230419134210_msel-pages.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright 2023 Carnegie Mellon University. All Rights Reserved.
+// Released under a MIT (SEI)-style license, please see LICENSE.md in the project root for license information or contact permission@sei.cmu.edu for full terms.
+
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
--- Blueprint.Api.Migrations.PostgreSQL/Migrations/20230420140753_msel-pages-ix-fix.cs
+++ Blueprint.Api.Migrations.PostgreSQL/Migrations/20230420140753_msel-pages-ix-fix.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿// Copyright 2023 Carnegie Mellon University. All Rights Reserved.
+// Released under a MIT (SEI)-style license, please see LICENSE.md in the project root for license information or contact permission@sei.cmu.edu for full terms.
+
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 

b77e35ddc5a2d5f7a44df16cbf613ccbce1ed522
sei-tspencer <trspencer@sei.cmu.edu>
2024-01-10 09:35:34 -0500

    add use player flag

---
+++ Blueprint.Api.Migrations.PostgreSQL/Migrations/20240110143101_use-player-flag.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blueprint.Api.Migrations.PostgreSQL.Migrations
+{
+    public partial class useplayerflag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "use_player",
+                table: "msels",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "use_player",
+                table: "msels");
+        }
+    }
+}

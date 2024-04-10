e905be0195031b168b24f9eae3d45078a62766f0
sei-tspencer <trspencer@sei.cmu.edu>
2024-04-10 14:50:00 -0400

    createdBy = msel owner

--- Blueprint.Api/Infrastructure/Authorization/MselOwnerRequirement.cs
+++ Blueprint.Api/Infrastructure/Authorization/MselOwnerRequirement.cs
@@ -14,23 +14,31 @@ namespace Blueprint.Api.Infrastructure.Authorization
     {
         public static async Task<Boolean> IsMet(Guid userId, Guid? mselId, BlueprintContext blueprintContext)
         {
-            var mselTeamIdList = await blueprintContext.Teams
-                .Where(t => t.MselId == mselId)
-                .Select(t => t.Id)
-                .ToListAsync();
-            var isSuccess = await blueprintContext.TeamUsers
-                .Where(tu => tu.UserId == userId && mselTeamIdList.Contains(tu.TeamId))
-                .AnyAsync();
-            if (isSuccess)
+            var createdBy = (await blueprintContext.Msels.FirstOrDefaultAsync(m => m.Id == mselId)).CreatedBy;
+            if (createdBy == userId)
             {
-                isSuccess = await blueprintContext.UserMselRoles
-                    .Where(umr => umr.UserId == userId &&
-                        umr.MselId == mselId &&
-                        umr.Role == Data.Enumerations.MselRole.Owner)
-                    .AnyAsync();
+                return true;
             }
+            else
+            {
+                var mselTeamIdList = await blueprintContext.Teams
+                    .Where(t => t.MselId == mselId)
+                    .Select(t => t.Id)
+                    .ToListAsync();
+                var isSuccess = await blueprintContext.TeamUsers
+                    .Where(tu => tu.UserId == userId && mselTeamIdList.Contains(tu.TeamId))
+                    .AnyAsync();
+                if (isSuccess)
+                {
+                    isSuccess = await blueprintContext.UserMselRoles
+                        .Where(umr => umr.UserId == userId &&
+                            umr.MselId == mselId &&
+                            umr.Role == Data.Enumerations.MselRole.Owner)
+                        .AnyAsync();
+                }
 
-            return isSuccess;
+                return isSuccess;
+            }
         }
     }
 }

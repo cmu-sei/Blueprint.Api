6df5653a11ff24ce82f03ff081b4ee98ab05997e
Mark Priest <mpriest@sei.cmu.edu>
2024-06-07 13:26:57 -0400

    Created new DB migration

---
+++ Blueprint.Api.Migrations.PostgreSQL/Migrations/20240607171929_rename-scenario-events-rowindex.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blueprint.Api.Migrations.PostgreSQL.Migrations
+{
+    public partial class renamescenarioeventsrowindex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "row_index",
+                table: "scenario_events",
+                newName: "group_order");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "group_order",
+                table: "scenario_events",
+                newName: "row_index");
+        }
+    }
+}
